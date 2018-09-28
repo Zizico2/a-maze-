4975dfd9372d8e3f6ff7837e550e0ee6c1c317b0
Bernardo António Borda d'Água <berna.agua@gmai.com>
2018-09-29 00:19:33 +0100

    Second Working Prototype
    Bounces working properly.
    Ball Rotation prototype.

--- Scripts/Map_Body.gd
+++ Scripts/Map_Body.gd
@@ -6,6 +6,7 @@ extends KinematicBody2D
 const gravity = 10;
 var gravity_multiplier = 1;
 var motion = Vector2(0,0);
+var rotation_force = 0;
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -13,10 +14,27 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	motion.y -= gravity * gravity_multiplier;
+	get_node("/root/world/Ball/Ball_Sprite").rotate(rotation_force);
 	if test_move(global_transform, delta*motion):
-		motion.y = motion.y * (-0.1); 
-	motion = move_and_slide(motion, Vector2(0,1));
+		var y = motion.y * (-0.1) + gravity * gravity_multiplier;
+		print("this");
+		motion = move_and_slide(motion, Vector2(0,-1));
+		motion.y = y;
+		
+	else:
+		motion.y -= gravity * gravity_multiplier;
+		motion = move_and_slide(motion, Vector2(0,-1));
+		
+	print(motion);
+	if is_on_ceiling() or is_on_wall():
+		motion -= motion*0.06;
+		print(motion.length());
+		
+		if motion.x > 0:
+			rotation_force = -motion.length()*0.0008;
+		elif motion.x < 0:
+			rotation_force = motion.length()*0.0008;
+	
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
